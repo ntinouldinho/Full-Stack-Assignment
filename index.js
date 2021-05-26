39d871c798509e66544433d103d68ca03a5a0704
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2021-05-26 13:23:18 +0300

    basic api

--- index.js
+++ index.js
@@ -1,22 +1,78 @@
 const express = require('express')
 const path = require('path')
+const favourites = require('./models/FavouriteBook')
 const app = express()
 const port = 8080
 
+var exphbs = require('express-handlebars')
+
+
 app.use('/static/', express.static(__dirname + '/public'))
 
-app.use(express.urlencoded({extended:false}))
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(express.urlencoded({ extended: false }))
 
 app.use(express.json())
 
-app.get('/',function (req, res){
+
+
+
+
+
+
+app.get('/', function(req, res) {
     let options = {
-        root: path.join(__dirname,'public')
+        root: path.join(__dirname, 'public')
     }
 
-    res.sendFile('index.html',options,function (err){
+    res.sendFile('index.html', options, function(err) {
         console.log(err)
     })
+
+    // res.render('example.handlebars',
+    // {
+    //     title: "okkkk"
+    // })
 })
+
+app.delete('/favourites/', async function(req, res) {
+
+    const deleteBook = await favourites.deleteById(req.body.id)
+    
+    res.status(201).send("ok");
+})
+
+app.post('/favourites/create', async function(req, res) {
+    console.log(req.body)
+    const create = await favourites.create(req.body.id,req.body.title,req.body.author)
+    
+    res.status(201).send(create.toString());
+})
+
+app.get('/favourites/', async function(req, res) {
+    // let favouriteBooks = await favourites.findAll();
+
+    let create = favourites.findById(26);
+    console.log(create + "dd")
+
+    // res.render('example.handlebars',
+    // {
+    //     title: "okkkk"
+    // })
+})
+
+
+app.get('/list/', async function(req, res) {
+    let favouriteBooks = await favourites.findAll();
+
+    res.render('books-list.handlebars',
+    {
+        title: "Book List",
+        books:favouriteBooks
+    })
+})
+
 
 app.listen(port, () => console.log(`🚀🚀🚀🚀🚀server is running on port 8080🚀🚀🚀`));

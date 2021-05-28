0a8859656126bc5f4a05ccf052cd5de8df2368c4
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2021-05-28 10:44:26 +0300

    delete book js

--- public/js/list.js
+++ public/js/list.js
@@ -29,5 +29,51 @@ function handleSubmit(event,id) {
 
 
 function filterResults(event){
+    const search = event.target.value.toUpperCase();
+
+    let tr = document.querySelectorAll('#results-table > tbody > tr');
+    let length = tr.length;
     
+    for (let i = 0; i < length; i++) {
+        title = tr[i].getElementsByTagName("td")[0];
+        author = tr[i].getElementsByTagName("td")[1];
+        
+        if (title || author) {
+          title = title.textContent || title.innerText;
+          author = author.textContent || author.innerText;
+          if ((title.toUpperCase().indexOf(search) > -1) || (author.toUpperCase().indexOf(search) > -1)) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+      }
+
+}
+
+function deleteBook(id){
+    let deleteHeader = new Headers();
+        deleteHeader.append('Content-Type', 'application/json');
+
+        let init = {
+            method: 'DELETE',
+            headers: deleteHeader,
+            body: JSON.stringify({ id: id })
+        }
+
+        fetch("/favourites/", init)
+            .then(response => response.text())
+            .then(response => {
+                switch (response) {
+                    case 'ok':
+                        alert("deleted")
+                        window.location.reload();
+                        break;
+                    default:
+                        alert("an error occurred")
+                }
+            })
+            .catch(error => {
+                console.log(error)
+            })
 }
